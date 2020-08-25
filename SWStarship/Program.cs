7012927c05ca1e0f80b31de5936cbb99e1e86556
KennethMercado <52854400+KennethMercado@users.noreply.github.com>
2020-08-25 16:18:23 +0100

    Added Solution and Source Code

---
+++ SWStarship/Program.cs
@@ -0,0 +1,53 @@
+﻿using StarWarsApiCSharp;
+using System;
+
+namespace SWStarship
+{
+    class Program
+    {
+        private const string USAGE_TEXT = "Enter a distance in mega lights (MGLT) <e.g. 1000000>: ";
+        private const string PROCESS_TEXT = "Please wait...";
+        private const string ERROR_MESSAGE = "Please key-in the correct mega lights number e.g. 1000000";
+        private const string DISPLAY_FORMAT_NAME = "Name:{0} - Total Amount Of Stops:";
+        private const string DISPLAY_FORMAT_TOTAL_AMOUNT_OF_STOPS = "{0}";
+        private const int PAGE_NUMBER = 1;
+        private const int PAGE_SIZE = 100;
+        
+        static void Main(string[] args)
+        {
+            IRepository<Starship> starshipRepo = new Repository<Starship>();
+            SWStarshipHelper objSWStarshipHelper = new SWStarshipHelper();
+
+            Console.Clear();
+            Console.WriteLine(USAGE_TEXT);
+            int.TryParse(Console.ReadLine(), out int inputMegaLightsTotal);
+
+            if (inputMegaLightsTotal != 0)
+            {
+                Console.WriteLine(Environment.NewLine + PROCESS_TEXT);
+
+                foreach (var starship in starshipRepo.GetEntities(PAGE_NUMBER, PAGE_SIZE))
+                {
+                    var originalColor = Console.ForegroundColor;
+
+                    Console.Write(Environment.NewLine + string.Format(DISPLAY_FORMAT_NAME, starship.Name));
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(string.Format(DISPLAY_FORMAT_TOTAL_AMOUNT_OF_STOPS
+                        , objSWStarshipHelper.CalculateStops(
+                            inputMegaLightsTotal
+                            , starship.Consumables
+                            , starship.MegaLights).ToString()));
+                    Console.ForegroundColor = originalColor;
+                }
+            }
+            else
+            {
+                Console.WriteLine(ERROR_MESSAGE);
+            }
+
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Press any key to close this window . . .");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ SWStarship/SWStarshipHelper.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+namespace SWStarship
+{
+    /// <summary>
+    /// Star Wars Helper
+    /// </summary>
+    public class SWStarshipHelper
+    {
+        const int UNIT_TYPE_NUMBER = 0;
+        const int UNIT_TYPE_NAME = 1;
+
+        /// <summary>
+        /// This will calculate how many stops to cover a certain distance.
+        /// Expected Output
+        ///     Y-wing: 74
+        ///     Millennium Falcon: 9
+        ///     Rebel Transport: 11
+        ///     
+        ///     1,000,000 / (1 * 7 (in Week) * 24 * 80) = 74.40
+        ///     1,000,000 / (2 * 30 (in Month) * 24 * 75) = 9.25
+        ///     1,000,000 / (6 * 30 (in Month) * 24 * 20) = 11.57
+        /// </summary>
+        /// <param name="megaLightsTotal">e.g. 1000000</param>
+        /// <param name="consumables">e.g. 1 week</param>
+        /// <param name="megaLights">e.g. 80</param>
+        /// <returns>Total Amount of Stops e.g. 74</returns>
+        public int CalculateStops(int megaLightsTotal, string consumables, string megaLights)
+        {
+            string[] consumablesSplit = consumables.Split(' ');
+            //Try to convert MegaLights to Integer
+            int.TryParse(megaLights, out int megaLightsNumber); ;
+
+            //Check if Consumable Format is correct
+            if (consumablesSplit.Length == 2 && megaLightsNumber != 0)
+            {
+                //Try to convert Consumables' Unit Number to Integer
+                int.TryParse(consumablesSplit[UNIT_TYPE_NUMBER], out int number);
+                int numberOfDays = GetDays(number, consumablesSplit[UNIT_TYPE_NAME]);
+
+                //Prevent Division by Zero
+                if (numberOfDays > 0)
+                {
+                    return megaLightsTotal / (GetDays(number, consumablesSplit[1]) * 24 * megaLightsNumber);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// This data can be query in the database to make it more flexible.
+        /// e.g. Quarterly, Semi-Annual, Century etc...
+        /// </summary>
+        /// <param name="unitTypeNumber"></param>
+        /// <param name="unitTypeName"></param>
+        /// <returns></returns>
+        int GetDays(int unitTypeNumber, string unitTypeName)
+        {
+            Dictionary<string, int> numberOfDays = new Dictionary<string, int>();
+
+            //Data will be coming from Database
+            numberOfDays.Add("DAYS", 1);
+            numberOfDays.Add("DAY", 1);
+
+            numberOfDays.Add("MONTHS", 30);
+            numberOfDays.Add("MONTH", 30);
+
+            numberOfDays.Add("YEARS", 365);
+            numberOfDays.Add("YEAR", 365);
+
+            numberOfDays.Add("WEEKS", 7);
+            numberOfDays.Add("WEEK", 7);
+
+
+            if (numberOfDays.ContainsKey(unitTypeName.ToUpper()))
+            {
+                return unitTypeNumber * numberOfDays[unitTypeName.ToUpper()];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
